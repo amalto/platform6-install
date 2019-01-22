74169b93952c5c3117a3da9519c5bcf5df1a76f7
Choucri FAHED <choucri.fahed@mines-nancy.org>
2019-01-22 19:12:56 +0100

    #3 Project name should remain platform6

--- stop_platform6.sh
+++ stop_platform6.sh
@@ -6,7 +6,7 @@ echo "*** Stopping Platform 6..."
 source .env
 export INSTANCE_DATA_PATH=$PLATFORM6_ROOT/$INSTANCE_ID
 
-docker-compose -f docker-compose.yaml -p p6core down
+docker-compose -f docker-compose.yaml -p platform6 down
 docker stop p6proxy
 docker stop p6core
 docker stop pgsql
@@ -15,5 +15,4 @@ docker stop ethstats
 docker stop demobc
 
 docker network ls
-docker network rm platform6_main
 docker network rm platform6_app_net
