d04e0e9f3e4145ba619cdfce2375c5bdf0d9c858
Choucri FAHED <choucri.fahed@mines-nancy.org>
2019-05-02 16:40:14 +0200

    Fixes #18

---
+++ cluster/start_platform6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo "*** Starting Platform 6..."
+
+if [ -e ../.env ]; then
+    source ../.env
+else
+    echo "Please set up your .env file before starting your environment."
+    exit 1
+fi
+
+export INSTANCE_DATA_PATH=$PLATFORM6_ROOT/$INSTANCE_ID
+
+docker-compose -f docker-compose.yaml -p platform6 up -d
