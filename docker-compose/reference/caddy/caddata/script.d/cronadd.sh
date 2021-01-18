5edbaebda7a76e16d581a261dce154ae80e64fdf
Simon Temple <simon.temple@amalto.com>
2021-01-18 09:33:18 +0000

    Simplified the two caddy deployments and removed the nginx equivalents

--- docker-compose/reference/caddy/caddata/script.d/cronadd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-echo "0 0 * * * /data/conf.d/buildbadrules.sh" > /etc/crontabs/root
-chmod +x /data/conf.d/buildbadrules.sh
-crond restart
--- docker-compose/reference/caddy/caddata/script.d/ensurelog.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Make sure the log file exists for fail2ban otherwise it will fail to start
-touch /data/logs/access.log
