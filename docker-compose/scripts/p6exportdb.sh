af8ccf0a1be443d741518be8b7c7be2dd7251763
Choucri FAHED <choucri.fahed@mines-nancy.org>
2021-03-11 18:28:05 +0100

    Make db exports more resilient

--- docker-compose/scripts/p6exportdb.sh
+++ docker-compose/scripts/p6exportdb.sh
@@ -4,7 +4,7 @@ if [ ! -d database_dumps ]; then
   mkdir database_dumps
 fi
 
-docker exec -t pgsql pg_dumpall -c -U postgres > ./database_dumps/dump_latest.sql
+docker exec -t pgsql pg_dumpall -c --if-exists -U postgres > ./database_dumps/dump_latest.sql
 cp ./database_dumps/dump_latest.sql ./database_dumps/dump_`date +%Y%m%d"_"%H%M%S`.sql
 
 echo "### Export Complete."
